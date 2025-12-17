1c61e85f5c5f18b5210a899bf533ec5b3defe613
omaralbalkhi444-code <omaralbalkhi444@gmail.com>
2025-12-17 05:57:49 +0300

    Update approveItem.ts

--- functions/src/items/approveItem.ts
+++ functions/src/items/approveItem.ts
@@ -1,11 +1,11 @@
 import { onCall } from "firebase-functions/v2/https";
-import { getFirestore, Timestamp } from "firebase-admin/firestore";
+import * as admin from "firebase-admin";
 
 export const approveItem = onCall(async (request) => {
   const itemId = request.data.itemId;
   if (!itemId) throw new Error("Missing itemId");
 
-  const db = getFirestore();
+  const db = admin.firestore();
 
   const pendingRef = db.collection("pending_items").doc(itemId);
   const itemsRef = db.collection("items").doc(itemId);
@@ -14,23 +14,54 @@ export const approveItem = onCall(async (request) => {
   if (!snap.exists) throw new Error("Pending item not found");
 
   const data = snap.data()!;
+  const ownerId = data.ownerId;
 
   await itemsRef.set({
     ...data,
-
     status: "approved",
-    approvedAt: Timestamp.now(),
-
-    // Add rating system ONLY when approved
+    approvedAt: admin.firestore.FieldValue.serverTimestamp(),
     rating: 0,
     reviews: [],
   });
 
-  // Update pending item metadata
   await pendingRef.update({
     status: "approved",
-    reviewedAt: Timestamp.now(),
+    reviewedAt: admin.firestore.FieldValue.serverTimestamp(),
   });
+
+  //  Save notification 
+  await db.collection("notifications").add({
+    senderId: "ADMIN",
+    receiverId: ownerId,
+    type: "ITEM_APPROVED",
+    itemId: itemId,
+    title: "Item Approved",
+    body: "Your item has been approved ",
+    isRead: false,
+    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+  });
+
+  //   FCM Realtime Database
+  const tokenSnap = await admin
+    .database()
+    .ref(`users/${ownerId}/fcmToken`)
+    .get();
+
+  const fcmToken = tokenSnap.val();
+
+  if (fcmToken) {
+    await admin.messaging().send({
+      token: fcmToken,
+      notification: {
+        title: "Item Approved",
+        body: "Your item has been approved ",
+      },
+      data: {
+        type: "ITEM_APPROVED",
+        itemId: itemId,
+      },
+    });
+  }
 
   return { success: true };
 });
