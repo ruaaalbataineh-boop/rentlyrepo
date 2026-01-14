36587a52959af113074fd9a7cf380784d374399b
Qusay_Alquraan <qusayalquraan03t@gmail.com>
2026-01-15 00:03:29 +0300

    Update user_manager.dart

--- lib/user_manager.dart
+++ lib/user_manager.dart
@@ -1,7 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class UserManager {
+  
+  static String? _testUid;
+
+  
+  static void setTestUid(String? uid) {
+    _testUid = uid;
+  }
+
   static String? get uid {
+    
+    if (_testUid != null) {
+      return _testUid;
+    }
+
     final user = FirebaseAuth.instance.currentUser;
     return user?.uid;
   }
